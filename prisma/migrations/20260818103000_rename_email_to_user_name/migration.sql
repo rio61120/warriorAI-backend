da4cf7be79358d31927f058b129a3f80aa8180bf
Rio Nguyen <ducnhse20@gmail.com>
2026-08-25 21:28:45 +0700

    remove migration

--- prisma/migrations/20260818103000_rename_email_to_user_name/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" RENAME COLUMN "email" TO "user_name";
-ALTER INDEX "users_email_key" RENAME TO "users_user_name_key";
--- prisma/migrations/20260820105915_implement_chat/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "conversations" ADD CONSTRAINT "conversations_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
